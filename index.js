979b7f7e4d173b6c3192ac53050597be3b9fda86
Noel Fernandez <noelfernandez98@gmail.com>
2021-09-29 14:50:54 +0000

    code challenge 9/29

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 //1
 function countToTen(){
-  
+    for(let i = 1; i < 11; i++){
+    console.log(i)
+    }
 }
+countToTen()
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+    for(let i = 1; i <= num; i++){
+  console.log(i)
 }
+}
+countFromOne(5)
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(num){
+  for(let i = 2; i <= num; i+=2){
+      console.log(i)
+  }
 }
-
+countEveryEven(9)
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+  for(let i = 1; i <= num; i+=2){
+  console.log(i)
 }
+}
+countEveryOdd(11)
 
 //5 
-function countEvens(){
-  
+function countEvens(num){
+    let count = 0
+    for (let i = 0; i < num.length; i++){
+        if(num[i] % 2 === 0){
+            count+=1
+        }
+}
+  return count
 }
 
+console.log(countEvens([13,12,5,9,3,8,1,2]));
